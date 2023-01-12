582bd9637c95132cb070e92512a3d7933615320e
LuisPimentel93 <120436164+LuisPimentel93@users.noreply.github.com>
2023-01-11 19:51:06 -0500

    changed the footer

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+let footer = document.querySelector('.footer')
+footer.style.color = 'red'
+footer.style.backgroundColor = 'black'
 
-// Select some elements...
+
+/*let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}*/
